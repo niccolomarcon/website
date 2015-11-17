6ccccd00a45a72fbc9a172b897d4c927780c9a21
Niccolò Marcon <marcon.niccolo@gmail.com>
2015-11-17 18:29:00 +0100

    done some stuff

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   $.getJSON('./js/quotes.json', function(data) {
     var random = Math.floor((Math.random() * data.length));
     $('p').text(data[random].quote);
-    $('footer').text(data[random].author);
+    $('blockquote > footer').text(data[random].author);
+    $('img').fadeIn('slow');
+    $('.wrapper').fadeIn('slow');
   });
-  $("img").fadeIn();
-  $(".wrapper").fadeIn();
 });
-
