138b5d778d59a2abc53ed88af83ca43373cffc88
Niccolò Marcon <marcon.niccolo@gmail.com>
2015-12-27 19:03:33 +0100

    Konami

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function() {
   
+  function writeQuote(quote, author) {
+    $('blockquote > p').text(quote);
+    $('blockquote > footer').text(author);
+    $('.wrapper').fadeIn('slow');
+  }
+  
+  function changeQuote(quote, author) {
+    $('.wrapper').fadeOut('slow', function() { writeQuote(quote, author); });
+  }
+  
+  // Loading the quote
   $.getJSON('js/quotes.json', function(data) {
     var random = Math.floor((Math.random() * data.length));
-    $('blockquote > p').text(data[random].quote);
-    $('blockquote > footer').text(data[random].author);
-    $('.wrapper').fadeIn('slow');
+    writeQuote(data[random].quote, data[random].author);
   });
   
+  // Loading the bg image
   var img = new Image();
-  
   img.onload = function() {
     $('.bg')
       .css('background-image', 'url(media/bg.jpg)')
@@ -17,6 +26,10 @@ $(document).ready(function() {
       .css('-moz-animation-play-state', 'running')
       .css('-o-animation-play-state', 'running');
   };
-  
   img.src = 'media/bg.jpg';
+  
+  // Someone said Konami code(?)
+  new Konamiz().onStart(function() { changeQuote('Gimme moar lifez!', 'Bill Rizer'); });
+  new Konamiz(['4', '2']).onStart(function() { changeQuote('The Ultimate Question of Life, the Universe and Everything', 'Deep Thought'); });
+  
 });
