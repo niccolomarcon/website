8645f6cf6bb8f45342f8d31627c1b154a332102a
Niccolò Marcon <marcon.niccolo@gmail.com>
2015-12-27 19:10:40 +0100

    correction of the 42 quote

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  
+
   function writeQuote(quote, author) {
     $('blockquote > p').text(quote);
     $('blockquote > footer').text(author);
     $('.wrapper').fadeIn('slow');
   }
-  
+
   function changeQuote(quote, author) {
     $('.wrapper').fadeOut('slow', function() { writeQuote(quote, author); });
   }
-  
+
   // Loading the quote
   $.getJSON('js/quotes.json', function(data) {
     var random = Math.floor((Math.random() * data.length));
     writeQuote(data[random].quote, data[random].author);
   });
-  
+
   // Loading the bg image
   var img = new Image();
   img.onload = function() {
@@ -27,9 +27,19 @@ $(document).ready(function() {
       .css('-o-animation-play-state', 'running');
   };
   img.src = 'media/bg.jpg';
-  
+
   // Someone said Konami code(?)
-  new Konamiz().onStart(function() { changeQuote('Gimme moar lifez!', 'Bill Rizer'); });
-  new Konamiz(['4', '2']).onStart(function() { changeQuote('The Ultimate Question of Life, the Universe and Everything', 'Deep Thought'); });
-  
+  new Konamiz().onStart(function() {
+    changeQuote(
+      'Gimme moar lifez!',
+      'Bill Rizer'
+    );
+  });
+  new Konamiz(['4', '2']).onStart(function() {
+    changeQuote(
+      'The Ultimate Question of Life, the Universe and Everything.',
+      'Deep Thought'
+    );
+  });
+
 });
